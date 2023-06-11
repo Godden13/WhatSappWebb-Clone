4a8b6e5b7269b9ffd24262090372eceb77eadc2f
Godden13 <chrissgodden@gmail.com>
2023-06-11 21:04:58 +0100

    add component page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,25 @@
 import { auth } from "@/fireBase/config";
-import { FacebookAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import {
+  FacebookAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext<any>(null);
 
 export const useAuth = () => useContext(AuthContext);
 
-export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const [user, setUser] = useState<any>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -26,19 +37,32 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
     return () => unsubscribe();
   }, []);
 
-
   const faceBookSignIn = () => {
     const provider = new FacebookAuthProvider();
     signInWithPopup(auth, provider)
       .then((res) => {
-        console.log(res)
+        console.log(res);
       })
       .catch((err) => {
-        console.log(err.message)
+        console.log(err.message);
+      });
+  };
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        router.push("/");
+        console.log("logOut successful");
       })
-  }
-  
+      .catch((err) => {
+        console.log(err + "  message");
+      });
+  };
+
   return (
-    <AuthContext.Provider value={{ user, faceBookSignIn}} > {isLoading ? null : children}</AuthContext.Provider>
-  )
-}
+    <AuthContext.Provider value={{ user, faceBookSignIn, logOut }}>
+      {" "}
+      {isLoading ? null : children}
+    </AuthContext.Provider>
+  );
+};
