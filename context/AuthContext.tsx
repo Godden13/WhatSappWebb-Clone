58fb1751c70d376279166ff5e465979c0b3a8c3a
Godden13 <chrissgodden@gmail.com>
2023-06-13 17:03:06 +0100

    fixing errors in merge

---
+++ context/AuthContext.tsx
@@ -0,0 +1,68 @@
+import {
+  FacebookAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../fireBase/config";
+
+const AuthContext = createContext<any>(null);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [user, setUser] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+        });
+      } else {
+        setUser(null);
+      }
+      setIsLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const faceBookSignIn = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        router.push("/");
+        console.log("logOut successful");
+      })
+      .catch((err) => {
+        console.log(err + "  message");
+      });
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, faceBookSignIn, logOut }}>
+      {" "}
+      {isLoading ? null : children}
+    </AuthContext.Provider>
+  );
+};
