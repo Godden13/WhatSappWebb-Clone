71761cf59b4f3592d037014ae2c1ef161e51f397
MALWICK <batahumphrey66@gamil.com>
2023-06-14 12:17:20 +0100

    created basic changes

---
+++ pages/Media/index.js
@@ -0,0 +1,13 @@
+"use client"
+
+import WhatsappInstruction from "../Components/WhatsappInstruction"
+
+const page = () => {
+  return (
+    <div>
+      <WhatsappInstruction/>
+    </div>
+  )
+}
+
+export default page
